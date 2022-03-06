0edcd72e01f6e0506b90e5b9e869a75c08067093
Stergios Papathanasiou <sts.papa@gmail.com>
2022-03-06 12:33:54 +0200

    Initial folder structure, fonts, etc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react';
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+   return (
+      <div>
+         <HomePage/>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Box} from "@mui/material";
+import classes from './HomePage.module.css';
+
+function HomePage() {
+    return (
+        <Box className={classes.container}>
+            Hello
+        </Box>
+    )
+}
+
+export default HomePage;
