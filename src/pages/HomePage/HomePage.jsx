16d2d795ba3d9a107cd24093cb6ac0288504fb84
Stergios Papathanasiou <sts.papa@gmail.com>
2022-03-09 09:24:44 +0200

    Basic HomePage architecture

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Box, Button, Grid, Typography} from "@mui/material";
 import classes from './HomePage.module.css';
 
 function HomePage() {
-    return (
-        <Box className={classes.container}>
-            Hello
-        </Box>
-    )
+
+   return (
+      <Box className={classes.container}>
+         <Grid container spacing={1}>
+            <Grid item lg={4} md={4}>
+               <Box className={classes.menuItem}>
+                  <Typography style={{fontFamily:'Gotham Bold', marginBottom: '5px'}}>HATS</Typography>
+                  <Button className={classes.button} disableRipple>SHOP NOW</Button>
+               </Box>
+            </Grid>
+            <Grid item lg={4} md={4}>
+               <Box className={classes.menuItem}>
+                  <Typography style={{fontFamily:'Gotham Bold', marginBottom: '5px'}}>JACKETS</Typography>
+                  <Button className={classes.button} disableRipple>SHOP NOW</Button>
+               </Box>
+            </Grid>
+            <Grid item lg={4} md={4}>
+               <Box className={classes.menuItem}>
+                  <Typography style={{fontFamily:'Gotham Bold', marginBottom: '5px'}}>SNEAKERS</Typography>
+                  <Button className={classes.button} disableRipple>SHOP NOW</Button>
+               </Box>
+            </Grid>
+         </Grid>
+         <Box mt={1} />
+         <Grid container spacing={1}>
+            <Grid item lg={6} md={6}>
+               <Box className={classes.menuItem}>
+                  <Typography style={{fontFamily:'Gotham Bold', marginBottom: '5px'}}>WOMENS</Typography>
+                  <Button className={classes.button} disableRipple>SHOP NOW</Button>
+               </Box>
+            </Grid>
+            <Grid item lg={6} md={6}>
+               <Box className={classes.menuItem}>
+                  <Typography style={{fontFamily:'Gotham Bold', marginBottom: '5px'}}>MENS</Typography>
+                  <Button className={classes.button} disableRipple>SHOP NOW</Button>
+               </Box>
+            </Grid>
+         </Grid>
+      </Box>
+   )
 }
 
 export default HomePage;
