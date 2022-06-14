a09965943ee8bb0265ec19acddccc9c9a1ffe1f4
Stergios Papathanasiou <sts.papa@gmail.com>
2022-06-14 17:06:20 +0300

    Initial sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "./pages/homepage";
 import ShopPage from "./pages/shop";
+import SignUpPage from './pages/sign-up/';
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/sign-up/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import { Typography } from "@mui/material";
+
+function SignUp() {
+  return (
+    <Fragment>
+      <Typography
+        variant={"h1"}
+        style={{ fontFamily: "Chronicle Light, sans-serif", fontSize: 40 }}
+      >
+        Sign Up
+      </Typography>
+    </Fragment>
+  );
+}
+
+export default SignUp;
