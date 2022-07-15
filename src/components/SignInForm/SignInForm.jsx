ace6aa42f0ea243788a94828d67c0441c7d42454
Stergios Papathanasiou <sts.papa@gmail.com>
2022-07-15 13:47:33 +0300

    Finished sign-in with email and password

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,128 +1,154 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 import {
-  Box,
-  Card,
-  CardContent,
-  Checkbox,
-  FormControlLabel,
-  Grid,
-  styled,
-  Typography,
+    Box,
+    Card,
+    CardContent,
+    Checkbox,
+    FormControlLabel,
+    Grid,
+    styled,
+    Typography,
 } from "@mui/material";
 
 import BasicTextField from "../@core/BasicTextField/BasicTextField";
 import Button from "../@core/Button/Button";
 import {
-  createAuthUserDoc,
-  signInWithGooglePopup,
+    createAuthUserDoc,
+    signInWithGooglePopup,
+    signInAuthUserWithEmailAndPassword,
 } from "../../utilities/firebase/firebase";
-import { ReactComponent as GoogleIcon } from "../../assets/24 - google icon.svg";
+import {ReactComponent as GoogleIcon} from "../../assets/24 - google icon.svg";
 
 const StyledCheckBox = styled(Checkbox)({
-  "&.MuiCheckbox-root": {
-    color: "#FFD700",
-  },
+    "&.MuiCheckbox-root": {
+        color: "#FFD700",
+    },
 });
 
-function SignInForm({ setAnchorEl }) {
-  const loginGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    if (response) {
-      const { user } = response;
-      const userDocRef = await createAuthUserDoc(user);
+function SignInForm({setAnchorEl}) {
+    const [formFields, setFormFields] = useState({email: "", password: ""});
+    const {email, password} = formFields;
 
-      setAnchorEl(null);
-    }
-  };
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormFields({...formFields, [name]: value})
+    };
 
-  return (
-    <Card style={{ width: 350, height: "100%" }}>
-      <CardContent style={{ padding: "16px 24px" }}>
-        <Typography
-          style={{
-            fontFamily: "Chronicle Light",
-            fontSize: 20,
-            color: "#2D2D2D",
-          }}
-        >
-          Sign In
-        </Typography>
-        <Box mt={3} />
-        <Grid container flexDirection={"column"} spacing={2}>
-          <Grid item>
-            <BasicTextField
-              label={"Email"}
-              variant={"outlined"}
-              fullWidth
-              placeHolder={"Email"}
-              adornment={false}
-            />
-          </Grid>
-          <Grid item>
-            <BasicTextField
-              label={"Password"}
-              variant={"outlined"}
-              fullWidth
-              placeHolder={"Password"}
-              adornment={true}
-            />
-          </Grid>
-          <Grid item>
-            <FormControlLabel
-              control={
-                <StyledCheckBox iconStyle={{ fill: "#FFD700" }} disableRipple />
-              }
-              label={
-                <Typography style={{ fontFamily: "Gotham Book", fontSize: 15 }}>
-                  Remember me
+    const handleClearForm = () =>
+        setFormFields({firstName: "", lastName: "", email: "", password: ""});
+
+    const loginGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        if (response) {
+            const {user} = response;
+            await createAuthUserDoc(user);
+            setAnchorEl(null);
+        }
+    };
+
+    const loginWithEmailAndPassword = async () => {
+        signInAuthUserWithEmailAndPassword(email, password).then((res) => {
+            handleClearForm();
+        }).catch((e) => {
+            console.log(e.message)
+        });
+    };
+
+    return (
+        <Card style={{width: 350, height: "100%"}}>
+            <CardContent style={{padding: "16px 24px"}}>
+                <Typography
+                    style={{
+                        fontFamily: "Chronicle Light",
+                        fontSize: 20,
+                        color: "#2D2D2D",
+                    }}
+                >
+                    Sign In
                 </Typography>
-              }
-            />
-          </Grid>
-          <Grid item>
-            <Button
-              title={"Sign In"}
-              fullWidth
-              variant={"contained"}
-              backgroundColor={"#FFD700"}
-              backgroundHover={"rgb(255, 167, 0)"}
-            />
-          </Grid>
-          <Grid item>
-            <Button
-              title={"SIGN IN WITH GOOGLE"}
-              variant={"contained"}
-              fullWidth
-              onClick={loginGoogleUser}
-              startIcon={<GoogleIcon />}
-            />
-          </Grid>
-          <Grid item>
-            <Typography style={{ fontSize: 14, fontFamily: "Gotham Book" }}>
-              Don't have a login with us?{" "}
-              <Link
-                to={"/sign-up"}
-                style={{
-                  fontFamily: "Gotham Book",
-                  color: "#2D2D2D",
-                  textDecoration: "none",
-                }}
-              >
+                <Box mt={3}/>
+                <Grid container flexDirection={"column"} spacing={2}>
+                    <Grid item>
+                        <BasicTextField
+                            label={"Email"}
+                            variant={"outlined"}
+                            fullWidth
+                            placeHolder={"Email"}
+                            name={"email"}
+                            value={email}
+                            onChange={handleChange}
+                            adornment={false}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <BasicTextField
+                            label={"Password"}
+                            variant={"outlined"}
+                            fullWidth
+                            placeHolder={"Password"}
+                            name={"password"}
+                            value={password}
+                            onChange={handleChange}
+                            adornment={true}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <FormControlLabel
+                            control={
+                                <StyledCheckBox iconStyle={{fill: "#FFD700"}} disableRipple/>
+                            }
+                            label={
+                                <Typography style={{fontFamily: "Gotham Book", fontSize: 15}}>
+                                    Remember me
+                                </Typography>
+                            }
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            title={"Sign In"}
+                            fullWidth
+                            variant={"contained"}
+                            backgroundColor={"#FFD700"}
+                            backgroundHover={"rgb(255, 167, 0)"}
+                            onClick={loginWithEmailAndPassword}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            title={"SIGN IN WITH GOOGLE"}
+                            variant={"contained"}
+                            fullWidth
+                            onClick={loginGoogleUser}
+                            startIcon={<GoogleIcon/>}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Typography style={{fontSize: 14, fontFamily: "Gotham Book"}}>
+                            Don't have a login with us?{" "}
+                            <Link
+                                to={"/sign-up"}
+                                style={{
+                                    fontFamily: "Gotham Book",
+                                    color: "#2D2D2D",
+                                    textDecoration: "none",
+                                }}
+                            >
                 <span
-                  style={{ fontWeight: "bold" }}
-                  onClick={() => setAnchorEl(null)}
+                    style={{fontWeight: "bold"}}
+                    onClick={() => setAnchorEl(null)}
                 >
                   SIGN UP.
                 </span>
-              </Link>
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
 }
 
 export default SignInForm;
--- src/utilities/firebase/firebase.js
+++ src/utilities/firebase/firebase.js
@@ -1,19 +1,20 @@
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from "firebase/auth";
 
 import {
-  getFirestore,
-  // doc is used to set a document instance
-  doc,
-  // Access documents data
-  getDoc,
-  // Set documents data
-  setDoc,
+    getFirestore,
+    // doc is used to set a document instance
+    doc,
+    // Access documents data
+    getDoc,
+    // Set documents data
+    setDoc,
 } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,12 +22,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAukYHWifUEZfp6CvLbrNo-vVZ8eiq9luE",
-  authDomain: "sp-clothing-db.firebaseapp.com",
-  projectId: "sp-clothing-db",
-  storageBucket: "sp-clothing-db.appspot.com",
-  messagingSenderId: "929989334769",
-  appId: "1:929989334769:web:28f35339390cb3862dfb50",
+    apiKey: "AIzaSyAukYHWifUEZfp6CvLbrNo-vVZ8eiq9luE",
+    authDomain: "sp-clothing-db.firebaseapp.com",
+    projectId: "sp-clothing-db",
+    storageBucket: "sp-clothing-db.appspot.com",
+    messagingSenderId: "929989334769",
+    appId: "1:929989334769:web:28f35339390cb3862dfb50",
 };
 
 // Initialize Firebase
@@ -36,45 +37,51 @@ const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: "select_account",
+    prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
-  signInWithPopup(auth, googleProvider);
+    signInWithPopup(auth, googleProvider);
 
 //Initialize Firestore DB
 const firestoreDB = getFirestore();
 
 //Create user inside Firestore
 export const createAuthUserDoc = async (user, additionalInfo = {}) => {
-  if (!user) return
+    if (!user) return
 
-  const userDocRef = doc(firestoreDB, "users", user?.uid);
-  const userData = await getDoc(userDocRef);
+    const userDocRef = doc(firestoreDB, "users", user?.uid);
+    const userData = await getDoc(userDocRef);
 
-  //Check if there is an existing document reference for that user
-  if (!userData.exists()) {
-    const { displayName, email } = user;
-    const createdAt = new Date();
+    //Check if there is an existing document reference for that user
+    if (!userData.exists()) {
+        const {displayName, email} = user;
+        const createdAt = new Date();
 
-    try {
-      await setDoc(userDocRef, {
-        displayName,
-        email,
-        createdAt,
-        ...additionalInfo
-      });
-    } catch (e) {
-      console.log(`Error when creating the user`, e.message);
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInfo
+            });
+        } catch (e) {
+            console.log(`Error when creating the user`, e.message);
+        }
     }
-  }
 
-  return userDocRef;
+    return userDocRef;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
+    if (!email || !password) return;
 
-  return await createUserWithEmailAndPassword(auth, email, password);
+    return await createUserWithEmailAndPassword(auth, email, password);
 };
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+}
